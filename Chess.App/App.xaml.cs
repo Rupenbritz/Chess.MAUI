0c89932e7c48be5ddb84f642fe45a17c98532b2b
Alexander Strachan <rupenbritz@live.se>
2022-10-18 10:02:22 +0200

    First Commit - Solution Template

---
+++ Chess.App/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Chess.App;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ Chess.App/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Chess.App;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
